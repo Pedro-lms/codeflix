2c4dbac5a8832c7d3250e4b6ffe882d0c4494f02
Pedro-lms <pedrodaluzmachado@gmail.com>
2023-05-26 22:03:23 -0300

    error in the theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import  * as React from 'react';
-
 import {createTheme} from "@mui/material";
 import {Box, ThemeProvider } from '@mui/system';
 import {Header} from "./components/Header";
 import { Layout } from './components/Layout';
+import {appThemedest} from './config/theme';
 
-const theme = createTheme({});
+//const theme = createTheme({});
 
 function App() {
   return (
-  <ThemeProvider theme={theme}>
+  <ThemeProvider theme={appThemedest}>
     <Box component="main"
     sx={{
       height: '100vh'
